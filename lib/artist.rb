e02a91d549d1510d3df1db00a4878f97cf1693b9
howard suarez <howardesuarez@gmail.com>
2020-09-11 03:24:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,28 @@
-class Artist
-  @@all = []
+cclass Artist 
+
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    self.class.all << self
+    @@all << self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.map {|song| song.genre}
   end
 
   def self.all
     @@all
   end
 
-  def new_song(title, genre)
-    Song.new(title, self, genre)
+  def new_song(song, genre)
+    Song.new(name, self, genre)
   end
 
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
 
-  def genres
-    songs.map do |song|
-      song.genre
-    end
-  end
-
-end  
+end 
