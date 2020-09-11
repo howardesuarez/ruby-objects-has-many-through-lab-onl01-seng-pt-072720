ec6ccea45807416ea580fcbe06c3276e65990e0c
howard suarez <howardesuarez@gmail.com>
2020-09-11 03:24:46 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,27 +1,27 @@
 class Doctor
-  @@all=[]
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    appointments.map {|appointment| appointment.patient}
+  end
 
   def self.all
     @@all
   end
 
-  attr_accessor :name, :appointments, :patient
-
-  def initialize(name)
-    @name = name
-    @appointments = []
-    @@all << self
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
   end
 
-  def new_appointment(date, patient)
-     appointment = Appointment.new(patient, date, self)
-     @appointments << appointment
-     appointment
-  end
-
-  def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
-  end
-end
+end 
